1a3368ada9ddfdd8332ec506ee8b75fb75abc807
Robin Saini <robinsingh7242@gmail.com>
2025-03-05 13:34:00 +0530

    test commit

---
+++ scripts/data_ingestion.py
@@ -0,0 +1,9 @@
+# scripts/data_ingestion.py
+
+def ingest_data():
+    # This function will eventually connect to Databricks,
+    # perform data ingestion from the source to Delta tables.
+    print("Data ingestion logic will go here.")
+
+if __name__ == "__main__":
+    ingest_data()
